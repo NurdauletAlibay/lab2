c5282e71f5af449b1d60c3310e8ecdccebaa31a8
NurdauletAlibay <alibay.n01@mail.ru>
2022-09-21 16:55:45 +0600

    python

--- python.py
+++ python.py
@@ -1,5 +1,6 @@
+ #Nurdaulet Alibay
  hour = 9 
-if hour < 12: #Егер 12-ден кіші болса
-    print("Good morning") 
+if hour < 5: #Егер 12-ден кіші болса
+    print("Good morning") #True
 else:
-    print("Godd night") 
+    print("Godd night") #False
